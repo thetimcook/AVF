af5efc1679f986ca63a7510542e2c08b8ae4f8ec
Tim Cook <redeemingdesign@gmail.com>
2013-02-28 22:10:24 -0600

    iOS final build

--- js/main.js
+++ js/main.js
@@ -14,7 +14,27 @@ $(document).ready(function(){
 	};
 	sizeChange();		
 	$(window).resize(sizeChange);
-
 	
+	$('#r1').click(function(){
+		if ($('#p1').css('display') === "none"){
+			$('#p1').css('display','block');
+		} else {
+			$('#p1').css('display','none');
+		};
+	});
+	$('#r2').click(function(){		
+		if ($('#p2').css('display') === "none"){
+			$('#p2').css('display','block');
+		} else {
+			$('#p2').css('display','none');
+		};
+	});
+	$('#r3').click(function(){		
+		if ($('#p3').css('display') === "none"){
+			$('#p3').css('display','block');
+		} else {
+			$('#p3').css('display','none');
+		};
+	});
 });
 
